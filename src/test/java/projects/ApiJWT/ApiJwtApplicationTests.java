f34d18b9f1e1726b5b0a45303c1a0241555b6b18
Adrian Rodrigo Marchan Collazos <adrian.march23@gmail.com>
2025-11-19 23:48:11 -0500

    Inicio de proyecto

---
+++ src/test/java/projects/ApiJWT/ApiJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.ApiJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
